88f131fc79a5a96e497cce6f27d0b0266862154c
Sarang-BS-6 <162328333+Sarang-BS-6@users.noreply.github.com>
2024-07-03 00:04:16 +0530

    Update chromeUtils.ts

--- src/chromeUtils.ts
+++ src/chromeUtils.ts
@@ -1,6 +1,6 @@
 /// <reference types="chrome" />
 
-// Function to get a query from Chrome's local storage
+
 export const getQueryFromStorage = (callback: (query: string | undefined) => void) => {
     if (typeof chrome !== 'undefined' && chrome.storage && chrome.storage.local) {
       chrome.storage.local.get(['query'], (result) => {
